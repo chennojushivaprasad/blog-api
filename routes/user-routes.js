54524637acd1cfcb49170bb659f52ad87101a600
Shiva Prasad <73731709+chennojushivaprasad@users.noreply.github.com>
2023-07-21 17:39:37 +0530

    Update user-routes.js

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,6 +2,10 @@ const express  = require( "express")
 const {getAllUser,signup,login, deleteUser} = require("../controllers/user-controller")
 const userRouter = express.Router();
 
+
+
+
+userRouter.get("/:id",getUser);
 userRouter.get("/",getAllUser);
 userRouter.post("/signup",signup)
 userRouter.post("/login",login)
