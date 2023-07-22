380ffa1f9ee8e8d24f56fee9b1dec091a7042a4b
Shiva Prasad <73731709+chennojushivaprasad@users.noreply.github.com>
2023-07-22 23:30:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
-
+// app.use(express.json()) application doesnt know what type of data is recieving require( req.body
+// so to tell the application that we are recieving json body and it will parse all data into json format
+app.use(cors())
+app.use(express.json());
                                                      
 const DATABASE = process.env.MONGO_URL;
 const PORT = process.env.PORT || 3005;
@@ -15,10 +18,7 @@ const app = express();
 
 //app.use() is a middle ware
 
-// app.use(express.json()) application doesnt know what type of data is recieving require( req.body
-// so to tell the application that we are recieving json body and it will parse all data into json format
-app.use(cors())
-app.use(express.json());
+
 
 
 app.use("/api/user", userRouter);
